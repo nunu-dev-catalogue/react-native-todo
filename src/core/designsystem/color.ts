aa290bc6cebff9e671e5f9701ff38856d34ce69d
HyunWoo Lee <l2hyunwoo@gmail.com>
2024-04-28 20:41:28 +0900

    Add color system

---
+++ src/core/designsystem/color.ts
@@ -0,0 +1,28 @@
+const white = '#ffffff';
+const black = '#000000';
+const gray500 = '#999999';
+const gray400 = '#ababab';
+const gray300 = '#cccccc';
+const gray200 = '#ebebeb';
+const gray100 = '#f6f6f6';
+
+const red = '#ff3434';
+const pink = '#ffd2d2';
+const blue = '#5182ff';
+const skyblue = '#e2efff';
+const lightblue = '#f8fbff';
+
+export const color = {
+  white,
+  black,
+  gray500,
+  gray400,
+  gray300,
+  gray200,
+  gray100,
+  red,
+  pink,
+  blue,
+  skyblue,
+  lightblue,
+};
