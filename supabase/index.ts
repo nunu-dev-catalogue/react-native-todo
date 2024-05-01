441bc342a76107020f6c2466c3de37a7dfa6ff80
HyunWoo Lee <l2hyunwoo@gmail.com>
2024-05-01 11:02:13 +0900

    Replace config with dotenv

--- supabase/index.ts
+++ supabase/index.ts
@@ -1,9 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
-import Config from 'react-native-config';
 import { Database } from './supabase.type.ts';
 import 'react-native-url-polyfill/auto';
 
-const supabaseUrl = Config.REACT_APP_SUPABASE_URL ?? '';
-const supabaseAnonKey = Config.REACT_APP_SUPABASE_KEY ?? '';
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL ?? '';
+const supabaseAnonKey = process.env.REACT_APP_SUPABASE_KEY ?? '';
 
 export default createClient<Database>(supabaseUrl, supabaseAnonKey);
