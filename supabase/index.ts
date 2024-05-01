f1687f5e9335d60d28a17f38702db67da5f7e624
HyunWoo Lee (Nunu Lee) <54518925+l2hyunwoo@users.noreply.github.com>
2024-05-01 10:45:19 +0900

    Update yarn to yarn berry (#2)
    * Update yarn to yarn berry
    
    fix
    
    * fix reset scripts

--- supabase/index.ts
+++ supabase/index.ts
@@ -1,6 +1,6 @@
+import { createClient } from '@supabase/supabase-js';
 import Config from 'react-native-config';
-import {createClient} from '@supabase/supabase-js';
-import {Database} from './supabase.type.ts';
+import { Database } from './supabase.type.ts';
 import 'react-native-url-polyfill/auto';
 
 const supabaseUrl = Config.REACT_APP_SUPABASE_URL ?? '';
