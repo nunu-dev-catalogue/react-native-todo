746293d38fdb2de91ef5397d2f00e12afb7405c1
HyunWoo Lee <l2hyunwoo@gmail.com>
2024-05-02 11:07:33 +0900

    Replace moti instead of reanimated3

--- src/feature/home/component/TaskList.tsx
+++ src/feature/home/component/TaskList.tsx
@@ -1,16 +1,13 @@
-import React, { useCallback } from 'react';
+import { View as MotiView, AnimatePresence, ScrollView } from 'moti';
+import React from 'react';
 import { Text, TouchableHighlight, TouchableOpacity, View } from 'react-native';
+import { LinearTransition } from 'react-native-reanimated';
 import { createStyleSheet, useStyles } from 'react-native-unistyles';
 import { Task } from '../../../core/type/task.ts';
 import NotChecked from '../../../assets/images/ic_task_checked_default.svg';
 import Checked from '../../../assets/images/ic_task_checked_done.svg';
 import Liked from '../../../assets/images/ic_liked_filled.svg';
 import NotLiked from '../../../assets/images/ic_liked_default.svg';
-import Animated, {
-  FadeInRight,
-  FadeOutRight,
-  LinearTransition,
-} from 'react-native-reanimated';
 
 type Props = {
   title: string;
@@ -32,52 +29,56 @@ function TaskList({
   onMutateLike,
 }: Props): React.JSX.Element {
   const { styles } = useStyles(stylesheet);
-  const itemRenderer = useCallback(
-    ({ item }: { item: Task }) => {
-      return (
-        <Animated.View
-          style={styles.itemContainer}
-          entering={FadeInRight}
-          exiting={FadeOutRight}
-          layout={LinearTransition.springify()}>
-          <View style={styles.itemHeader}>
-            <TouchableHighlight
-              onPress={() => {
-                onMutateComplete({ id: item.id, completed: !item.completed });
-              }}>
-              <View>{item.completed ? <Checked /> : <NotChecked />}</View>
-            </TouchableHighlight>
-            <View style={styles.itemHeaderSpacer} />
-            <Text style={styles.itemTitle}>{item.title}</Text>
-          </View>
-          <TouchableOpacity
-            onPress={() => onMutateLike({ id: item.id, like: !item.like })}>
-            <View>{item.like ? <Liked /> : <NotLiked />}</View>
-          </TouchableOpacity>
-        </Animated.View>
-      );
-    },
-    [
-      onMutateComplete,
-      onMutateLike,
-      styles.itemContainer,
-      styles.itemHeader,
-      styles.itemHeaderSpacer,
-      styles.itemTitle,
-    ],
-  );
 
   return (
     <View style={styles.listContainer}>
       <Text style={styles.title}>{title}</Text>
       <View style={styles.spacer} />
-      <Animated.FlatList
-        skipEnteringExitingAnimations
-        renderItem={itemRenderer}
-        itemLayoutAnimation={LinearTransition.springify()}
-        keyExtractor={item => item.id.toString()}
-        data={tasks}
-      />
+      <ScrollView style={{ flex: 1 }}>
+        {tasks.map(item => (
+          <AnimatePresence>
+            <MotiView
+              key={item.id}
+              layout={LinearTransition}
+              from={{
+                opacity: 0,
+                translateX: 26,
+              }}
+              animate={{
+                opacity: 1,
+                translateX: 0,
+              }}
+              exit={{
+                opacity: 0,
+                translationX: -26,
+              }}
+              onDidAnimate={(key, finished) => {
+                if (finished) {
+                  console.log('Animation completed', key);
+                }
+              }}
+              style={styles.itemContainer}>
+              <View style={styles.itemHeader}>
+                <TouchableHighlight
+                  onPress={() => {
+                    onMutateComplete({
+                      id: item.id,
+                      completed: !item.completed,
+                    });
+                  }}>
+                  <View>{item.completed ? <Checked /> : <NotChecked />}</View>
+                </TouchableHighlight>
+                <View style={styles.itemHeaderSpacer} />
+                <Text style={styles.itemTitle}>{item.title}</Text>
+              </View>
+              <TouchableOpacity
+                onPress={() => onMutateLike({ id: item.id, like: !item.like })}>
+                <View>{item.like ? <Liked /> : <NotLiked />}</View>
+              </TouchableOpacity>
+            </MotiView>
+          </AnimatePresence>
+        ))}
+      </ScrollView>
     </View>
   );
 }
